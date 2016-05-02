50151e90d1cb4e201634a5cd8c517d4782005a2f
corneliusio <guy@cornelius.io>
2016-05-02 12:31:37 -0500

    support for older versions of node (>=0.12.0)

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ var fs = require('fs'),
 module.exports = function(options) {
 
     var stream = new Stream.Transform({objectMode: true}),
-        settings = Object.assign({
+        settings = {
             namespace: false,
             algorithm: 'sha1',
             file: '.checksums',
             fileIndent: 4
-        }, (typeof options === 'string') ? {namespace: options} : options);
+        };
+
+    options = (typeof options === 'string') ? {namespace: options} : options;
+
+    for (var key in options) {
+        settings[key] = options[key];
+    }
 
     if (settings.file) {
 
@@ -33,44 +39,41 @@ module.exports = function(options) {
     stream._transform = function(file, encoding, next) {
         var flow = this;
 
-        if (!file.checksum) {
+        if (file.isStream()) {
+            flow.push(file);
+            return next();
+        }
 
-            if (file.isStream()) {
-                flow.push(file);
-                return next();
+        if (file.isBuffer()) {
+
+            var filename = path.basename(file.path),
+                filechecksum = crypto
+                    .createHash(settings.algorithm || 'sha1')
+                    .update(file.contents.toString('utf8'))
+                    .digest('hex');
+
+            if (settings.namespace in oncechecksums) {
+
+                if (oncechecksums[settings.namespace][filename] === filechecksum) {
+                    return next();
+                }
+
+                oncechecksums[settings.namespace][filename] = filechecksum;
+            } else {
+
+                if (oncechecksums[filename] === filechecksum) {
+                    return next();
+                }
+
+                oncechecksums[filename] = filechecksum;
             }
 
-            if (file.isBuffer()) {
-
-                var filename = path.basename(file.path),
-                    filechecksum = crypto
-                        .createHash(settings.algorithm || 'sha1')
-                        .update(file.contents.toString('utf8'))
-                        .digest('hex');
-
-                if (settings.namespace in oncechecksums) {
-
-                    if (oncechecksums[settings.namespace][filename] === filechecksum) {
-                        return next();
+            if (settings.file) {
+                fs.writeFile(settings.file, JSON.stringify(oncechecksums, null, settings.fileIndent), function(error) {
+                    if (error) {
+                        flow.emit('error', error).bind(flow);
                     }
-
-                    oncechecksums[settings.namespace][filename] = filechecksum;
-                } else {
-
-                    if (oncechecksums[filename] === filechecksum) {
-                        return next();
-                    }
-
-                    oncechecksums[filename] = filechecksum;
-                }
-
-                if (settings.file) {
-                    fs.writeFile(settings.file, JSON.stringify(oncechecksums, null, settings.fileIndent), function(error) {
-                        if (error) {
-                            flow.emit('error', error).bind(flow);
-                        }
-                    });
-                }
+                });
             }
         }
 
